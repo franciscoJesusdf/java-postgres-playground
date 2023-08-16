01f4258f39cac5eb7d63a6e43609842125ac60b5
francisco Jesusdf <Francisco.jesusdf@gmail.com>
2023-08-16 03:59:13 +0000

    Add-Arrays.Java

---
+++ src/test/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class App {
+  
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println("MInha Aplicação Java\n");  
+        
+        // TIpo nomeDaVariavel;
+        var idade = 10;
+        var  peso = 80.55;
+        System.out.println("Idade é " + idade + " Peso é " + peso); 
+        var sexo = 'M';
+        System.out.println("Sexo: " + sexo);
+        var nessecidadesEspeciais = false;
+        System.out.println("Tem necessecidades especiais: " + nessecidadesEspeciais);
+
+        }      
+    }
---
+++ src/test/java/com/example/AppArrays.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+    public static void main(String[] args) {
+        int x = 10 ;
+        System.out.println(x);
+    
+        x = 20;
+
+        System.out.println(x);
+        double vetorDouble [] = {10, 20, 30};
+        System.out.println(Arrays.toString(vetorDouble)); 
+
+    }
+    
+}
