eb2e9639decfa0e472fe2c6f8e5549a88edbd631
francisco Jesusdf <Francisco.jesusdf@gmail.com>
2023-08-20 20:31:42 +0000

    edt changes

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -4,15 +4,25 @@ import java.util.Arrays;
 
 public class AppArrays {
     public static void main(String[] args) {
-        int x = 10 ;
-        System.out.println(x);
+        int vetor3[] = new int[5] ;
+       for (int i = 0; i < vetor3.length; i++) {
+        vetor3[i] = 100 * (i + 1);
+       }
+        System.out.println(Arrays.toString(vetor3));
+        System.out.println();
     
-        x = 20;
+        double matriz [] [] = new double[3] [4];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (i *  matriz[i].length + j+1) * 10;      
+            }
+        }
 
-        System.out.println(x);
-        double vetorDouble [] = {10, 20, 30};
-        System.out.println(Arrays.toString(vetorDouble)); 
-
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("%5.1f ", matriz[i][j]);           
+            }
+            System.err.println();
+        }
     }
-    
 }
